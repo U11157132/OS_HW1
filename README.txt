88094b33151222f42e8f8723a7245a2f9a584348
U11157132 <U11157132@o365.mcut.edu.tw>
2026-02-25 08:43:01 +0800

    Add files via upload

---
+++ README.txt
@@ -0,0 +1,4 @@
+作業名稱：第一次練習
+使用板子：Raspberry Pi Pico
+功能說明：
+此程式使用USB輸出Hello訊息
---
+++ hello_usb.c
@@ -0,0 +1,18 @@
+/**
+ * Copyright (c) 2020 Raspberry Pi (Trading) Ltd.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#include <stdio.h>
+#include "pico/stdlib.h"
+
+int main() {
+    stdio_init_all();
+    while (true) {
+        printf("Hello, world! U11157132 黃士維\n");
+        sleep_ms(1000);
+    }
+}
+
+//U11157132 黃士維
